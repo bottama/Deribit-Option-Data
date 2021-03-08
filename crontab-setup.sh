6cfd7fc48133e42a35503e9917e368d5d683b42b
Matteo Bottacini <matteo.bottacini@usi.ch>
2021-03-09 00:58:23 +0100

    Update and rename crontab.sh to crontab-setup.sh

--- crontab-setup.sh
+++ crontab-setup.sh
@@ -1,4 +1,4 @@
-# Author Matteo Bottacini, matteo.bottacini@usi.ch
+# Author: Matteo Bottacini, matteo.bottacini@usi.ch
 # Last update: March 8, 2021
 
 # open the crontab from your terminal by typing "crontab -e" and then copy and paste this commands.
@@ -10,16 +10,16 @@
 58 23 * * * sqlite3 -header -csv /home/bottama/deliverables/option-data-5min.db "select * from btc_option_data;" > csv_files/btc_option_data.csv
 58 23 * * * sqlite3 -header -csv /home/bottama/deliverables/option-data-5min.db "select * from eth_option_data;" > csv_files/eth_option_data.csv
 
-# initialize the data base at midnight
-0 0 * * * rm /home/bottama/deliverables/option-data-5min.db
+# initialize the data base at 23:59
+59 23 * * * rm /home/bottama/deliverables/option-data-5min.db
 
 # zip the csv file at 00.05
-5 0 * * * zip -9 -r btc_option_data.zip ./csv_files/btc_option_data
-5 0 * * * zip -9 -r eth_option_data.zip ./csv_files/eth_option_data
+5 0 * * * zip -9 -r btc_option_data.zip ./csv_files/btc_option_data.csv
+5 0 * * * zip -9 -r eth_option_data.zip ./csv_files/eth_option_data.csv
 
 # move the zipped files to the zip_files folder and rename it with timestamp at 00.15
-15 0 * * * mv csv_files/btc_option_data.zip zip_files/btc_option_data/$(date -d "today" +"%Y%m%d%H%M").zip
-15 0 * * * mv csv_files/eth_option_data.zip zip_files/eth_option_data/$(date -d "today" +"%Y%m%d%H%M").zip
+15 0 * * * mv btc_option_data.zip zip_files/btc_option_data/$(date -d "today" +"%Y%m%d").zip
+15 0 * * * mv eth_option_data.zip zip_files/eth_option_data/$(date -d "today" +"%Y%m%d").zip
 
 # remove the excel files at 00.30
 30 0 * * * rm /home/bottama/csv_files/btc_option_data.csv
